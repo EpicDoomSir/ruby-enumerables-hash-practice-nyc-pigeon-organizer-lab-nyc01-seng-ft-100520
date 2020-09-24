663fc8d80434879fa39be2cf91e9e5f777d46aa3
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-24 14:29:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  binding.pry
+  
   data2 = data.each_with_object do |(outer_key, outer_value), new_data|
+    binding.pry
     outer_value.each do |inner_key, names|
       names.each do |name|
         if !new_data[name]
